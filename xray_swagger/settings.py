b055a8c1c8dec5b2591d1bf2ad667718bb71fe9b
Hwal Choe <hwal.choe@nextlab.co.kr>
2023-06-28 19:36:01 +0900

    Fix aerich db init

--- xray_swagger/settings.py
+++ xray_swagger/settings.py
@@ -41,7 +41,7 @@ class Settings(BaseSettings):
     log_level: LogLevel = LogLevel.INFO
 
     # Variables for the database
-    db_host: str = "localhost"
+    db_host: str = "xray_swagger-db"
     db_port: int = 5432
     db_user: str = "xray_swagger"
     db_pass: str = "xray_swagger"
