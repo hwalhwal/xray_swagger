a7ebb95df70fc4c587f3d2606e445c6fe69cab9f
Hwal Choe <hwal.choe@nextlab.co.kr>
2023-09-01 16:06:46 +0900

    Extract common method DAO

--- xray_swagger/db/dao/_base.py
+++ xray_swagger/db/dao/_base.py
@@ -1,11 +1,71 @@
+from typing import Generic, Sequence, TypeVar
+
 from fastapi import Depends
+from loguru import logger
+from pydantic import BaseModel
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from xray_swagger.db.base import Base
 from xray_swagger.db.dependencies import get_db_session
 
+ModelType = TypeVar("ModelType", bound=Base)
+CreateDTOType = TypeVar("CreateDTOType", bound=BaseModel)
+UpdateDTOType = TypeVar("UpdateDTOType", bound=BaseModel)
 
-class DAOBase:
+
+class DAOBase(Generic[ModelType, CreateDTOType, UpdateDTOType]):
     """Base Class for DAO."""
+
+    model = ModelType
 
     def __init__(self, session: AsyncSession = Depends(get_db_session)) -> None:
         self.session = session
+
+    async def create(self, payload: CreateDTOType) -> ModelType:
+        logger.debug(f"=== CREATE {ModelType.__name__}")
+        async with self.session.begin_nested():
+            new_product = ModelType(**payload.model_dump(exclude_none=True))
+            self.session.add(new_product)
+        return new_product
+
+    async def get(self, id: int) -> ModelType | None:
+        logger.debug(f"=== GET 1 {ModelType.__name__}")
+        raw = await self.session.execute(
+            select(self.model).where(self.model.id == id),
+        )
+        return raw.scalar()
+
+    async def get_all(self) -> Sequence[ModelType]:
+        logger.debug(f"=== GET ALL {ModelType.__name__}")
+        raw = await self.session.execute(select(ModelType))
+        return raw.scalars().fetchall()
+
+    async def update(
+        self,
+        db_obj: ModelType,
+        payload: UpdateDTOType,
+        *,
+        exclude_none: bool = False,
+        exclude_unset: bool = False,
+    ) -> None:
+        refined_update_fields = payload.model_dump(
+            exclude_none=exclude_none,
+            exclude_unset=exclude_unset,
+        )
+        logger.debug(f"{refined_update_fields=}")
+        for k in refined_update_fields.keys():
+            logger.debug(f"{type(db_obj).__name__}.{k} = {db_obj.__getattribute__(k)}")
+        # UPDATE FIELDS
+        async with self.session.begin_nested():
+            logger.debug(f"=== UPDATE {type(db_obj).__name__}")
+            for k, v in refined_update_fields.items():
+                db_obj.__setattr__(k, v)
+
+        for k in refined_update_fields.keys():
+            logger.debug(f"{type(db_obj).__name__}.{k} = {db_obj.__getattribute__(k)}")
+
+    async def delete(self, db_obj: ModelType) -> None:
+        logger.debug(f"=== DELETE {type(db_obj).__name__}")
+        async with self.session.begin_nested():
+            await self.session.delete(db_obj)
--- xray_swagger/web/api/mmap_session/views.py
+++ xray_swagger/web/api/mmap_session/views.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 from uuid import UUID
 
 from fastapi import APIRouter, HTTPException, status
@@ -11,10 +12,10 @@ from .schema import MmapSessionCreateDTO, MmapSessionDTO, MmapSessionUpdateDTO
 router = APIRouter()
 
 
-@router.get(path="/sessions", response_model=list[MmapSessionDTO])
+@router.get(path="/sessions")
 async def get_all_mmap_sessions(
     dao: MmapSessionDAO = Depends(),
-):
+) -> Sequence[MmapSessionDTO]:
     d = await dao.filter()
     if not d:
         raise HTTPException(status.HTTP_404_NOT_FOUND, "Nothing.")
@@ -22,47 +23,59 @@ async def get_all_mmap_sessions(
     return d
 
 
-@router.get(path="/sessions/{uuid}", response_model=MmapSessionDTO)
+@router.get(path="/sessions/{uuid}")
 async def get_mmap_session_by_uuid(
     uuid: UUID,
     dao: MmapSessionDAO = Depends(),
-):
+) -> MmapSessionDTO:
     d = await dao.get(uuid)
     if not d:
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"MMAP session <uuid: {uuid}> Not Found")
     return d
 
 
-@router.post(path="/sessions", status_code=status.HTTP_201_CREATED, response_model=MmapSessionDTO)
+@router.post(path="/sessions", status_code=status.HTTP_201_CREATED)
 async def create_mmap_session(
     payload: MmapSessionCreateDTO,
     dao: MmapSessionDAO = Depends(),
-):
+) -> MmapSessionDTO:
     logger.debug(payload)
     logger.debug(payload.model_dump(exclude_none=True))
     new = await dao.create(payload)
     return new
 
 
-@router.patch(path="/sessions/{uuid}", response_model=MmapSessionDTO)
+@router.patch(path="/sessions/{uuid}")
 async def update_mmap_session(
     uuid: UUID,
     payload: MmapSessionUpdateDTO,
     dao: MmapSessionDAO = Depends(),
-):
+) -> MmapSessionDTO:
     d = await dao.get(uuid)
     if not d:
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"MMAP session <uuid: {uuid}> Not Found")
 
-    await dao.update(d, payload)
+    await dao.update(d, payload, exclude_none=True)
     return d
 
 
-@router.delete(path="/sessions/{uuid}", response_description="The mmap session has been removed")
+@router.delete(
+    path="/sessions/{uuid}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_description="The mmap session has been removed",
+)
 async def delete_mmap_session(
     uuid: UUID,
     dao: MmapSessionDAO = Depends(),
-):
+) -> None:
+    # noqa: E501
+    # TODO: sqlalchemy.exc.IntegrityError: (sqlalchemy.dialects.postgresql.asyncpg.IntegrityError)
+    # <class 'asyncpg.exceptions.ForeignKeyViolationError'>: update or delete
+    # on table "mmap_session" violates foreign key constraint
+    # "inspection_session_end_mmap_session_uuid_fkey"
+    # on table "inspection_session"
+    #  DETAIL:  Key (uuid)=(76f85d2a-ccf4-4a9e-bd92-f98135d9e164) is still referenced from table
+    # "inspection_session".
     d = await dao.get(uuid)
     if not d:
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"MMAP session <uuid: {uuid}> Not Found")
